9daabd5acbd770d06491d000cbd43d1f6512ad0f
liurui12w <978851037@qq.com>
2020-10-10 12:45:57 +0800

    fix: 三级子分类 修复

--- app/models/sub_category.rb
+++ app/models/sub_category.rb
@@ -2,4 +2,5 @@ class SubCategory < Category
     default_scope -> { where("parent_id is not null and category_type = ?","SubCategory") }
     has_many :providers
     belongs_to :main_category, foreign_key: :parent_id
+    has_many :item_categories, foreign_key: :parent_id
 end
